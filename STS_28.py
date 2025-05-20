ad86683ffb10f76660e58c456bced51d69d2bde4
kayy01 <kadjeiyeboah96@gmail.com>
2025-05-20 12:49:28 +0000

    initial commit

---
+++ STS_28.py
@@ -0,0 +1,17 @@
+#APIs(stands for Application Programming Interface): set of rules and tools that allow 2 systems(software application) to communicate with each other
+
+#types of APIs
+# 1. Rest 2. GraphQL 3. Soap
+
+
+#REST
+#Resource-based
+# Communicates with systems using HTTP. 
+# Request methods: GET = retrieve info POST = create data PUT= update data(complete) PATCH = update data(partial) DELETE= delete info
+#type of data used to interact is json. json is similar to dict in py but entirely different
+#
+#
+#
+#
+
+
